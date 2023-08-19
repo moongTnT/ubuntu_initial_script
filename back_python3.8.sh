18f2d98b9393e6ea4ccc30135394ce22096f1699
moongTnT <ansrms17@naver.com>
2023-08-19 14:08:45 +0900

    commit2

--- back_python3.8.sh
+++ back_python3.8.sh
@@ -1,10 +1,18 @@
-sudo apt update
+sudo apt update -y
 
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 
-sudo apt install python3.8
+sudo apt install python3.8 -y
 
 python3.8 --version
+
+sudo apt install python3-pip -y
+
+python3.8 -m pip --version
+
+python3.8 -m pip install --upgrade pip
+
+
 
--- front_node17_npm8_forever.sh
+++ front_node17_npm8_forever.sh
@@ -1,6 +1,6 @@
 curl -sL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 
-sudo apt install nodejs
+sudo apt install nodejs -y
 
 node --version
 npm --version
